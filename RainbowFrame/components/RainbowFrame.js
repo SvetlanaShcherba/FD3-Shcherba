22fc1b0672bb8df82188372d09fe613adcfed16e
SvetlanaShcherba <shcherba.svetlana4@gmail.com>
2023-01-20 22:03:08 +0300

    1

---
+++ RainbowFrame/components/RainbowFrame.js
@@ -0,0 +1,28 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+
+import './RainbowFrame.css';
+
+class RainbowFrame extends React.Component {
+
+static propTypes = {
+  colors: PropTypes.array.isRequired   
+};
+
+
+    
+  
+  render() {
+    let code = this.props.children;
+    for (let color of this.props.colors) {
+      code = <div style={{border: `solid 6px  ${color}`, padding:"7px"}}>{code}</div>
+    }
+    return code;
+          
+  }
+}
+
+export default RainbowFrame;
+
+ 
