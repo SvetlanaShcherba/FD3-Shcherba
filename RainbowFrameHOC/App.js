9186eeaad33c2cc6d5b8f0041bae69ab321e3e12
SvetlanaShcherba <shcherba.svetlana4@gmail.com>
2023-01-24 17:57:54 +0300

    1

---
+++ RainbowFrameHOC/App.js
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import RainbowFrame from './components/RainbowFrame';
+import DoubleButton from './components/DoubleButton';
+
+let colors = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
+
+ReactDOM.render(
+  <RainbowFrame colors={colors}>
+      <DoubleButton caption1="однажды" caption2="пору" cbPressed={ num => alert(num) }>в студёную зимнюю</DoubleButton>
+  </RainbowFrame>
+  , document.getElementById('container')
+);
+
