954193d8211d48bf5a043678cca771835efe0aa9
SvetlanaShcherba <shcherba.svetlana4@gmail.com>
2023-02-19 20:16:24 +0300

    3

--- BoxShop/src/pages/PageBasket.js
+++ BoxShop/src/pages/PageBasket.js
@@ -4,19 +4,21 @@ import { useSelector } from 'react-redux';
 export const PageBasket = () => {
   const item = useSelector(state => state.countItems);
   
+
+  let sum = ((item.countItems) * parseInt(item.price)) ? ((item.countItems) * parseInt(item.price)) : 0;
    
     return (
       <table className='Items'>
         <tbody>
           <tr><td>Наименование</td>
-            <td>Стоимость/шт</td>
+            <td>Стоимость</td>
             <td>Количество</td>
-            <td>Общая стоимость</td></tr>
+            <td>Сумма</td></tr>
           <tr>
             <td>{item.name}</td>
             <td>{item.price}</td>
             <td>{item.countItems}</td>
-            <td>{(item.countItems)*parseInt(item.price)}</td>
+            <td>{sum}</td>
           </tr>
         </tbody>
       </table>
